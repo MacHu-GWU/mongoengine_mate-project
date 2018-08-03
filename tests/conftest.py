13e69975ae82e602f64c59d19e8ebf9d0229467c
Sanhe Hu <sanhehu@Sanhes-MacBook.fios-router.home>
2018-08-02 21:16:12 -0400

    bug fix test database

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import sys
 import pytest
 import mongoengine
 
@@ -12,8 +13,13 @@ def connect():
     password = "&2z7#tMH6BJt"
     host = "mongodb://{username}:{password}@ds113063.mlab.com:13063/devtest".\
         format(username=username, password=password)
-    conn = mongoengine.connect(
+    client = mongoengine.connect(
         dbname,
         host=host,
     )
-    conn.drop_database(dbname)
+    db = client[dbname]
+
+    py_ver = "%s.%s" % (sys.version_info.major, sys.version_info.minor)
+    for col_name in db.list_collection_names():
+        if col_name.endswith(py_ver):
+            db[col_name].remove({})
